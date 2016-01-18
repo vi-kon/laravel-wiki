13bb460f23c4cf743da1f5ce49e8283108468a42
Kovács Vince <vincekovacs@hotmail.com>
2016-01-18 04:52:56 +0100

    Fixed page preview modal dialog

--- src/resources/views/page/create-modal-preview.blade.php
+++ src/resources/views/page/create-modal-preview.blade.php
@@ -1,5 +1,6 @@
-@extends('bootstrap::modal/modal')
+@extends('vi-kon.bootstrap::modal.modal-form')
 
+<?php $onlyContent = true ?>
 
 @section('title')
     <i class="icon-io-eye"></i>
@@ -9,18 +10,16 @@
 
 
 @section('body')
-    {!!$content!!}
+    {!! $content !!}
 @overwrite
 
 
 @section('footer')
-    <div class="modal-footer">
-        <button type="button" class="btn btn-primary"
-                data-loading-text="@lang('wiki::page/create.modal.preview.btn.save.loading')">
-            @lang('wiki::page/create.modal.preview.btn.save.content')
-        </button>
-        <button type="button" class="btn btn-default" data-dismiss="modal">
-            @lang('wiki::page/create.modal.preview.btn.back.content')
-        </button>
-    </div>
+    <button type="button" class="btn btn-primary"
+            data-loading-text="@lang('wiki::page/create.modal.preview.btn.save.loading')">
+        @lang('wiki::page/create.modal.preview.btn.save.content')
+    </button>
+    <button type="button" class="btn btn-default" data-dismiss="modal">
+        @lang('wiki::page/create.modal.preview.btn.back.content')
+    </button>
 @overwrite
